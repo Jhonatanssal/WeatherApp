c13f8fe5c53e498062e7cd30ed8b52d79073df04
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-11-06 22:34:07 -0500

    Add API Icon DOM manipulation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import './style.scss';
+
+window.addEventListener('load', () => {
+  let long;
+  let tempDescription = document.querySelector('.temperature-description');
+  let tempDegree = document.querySelector('.temperature-degree');
+  let locationTimeZone = document.querySelector('.location-timezone');
+  let icon = document.getElementById('icon');
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = 'medellin';
+
+      const proxy = 'http://cors-anywhere.herokuapp.com/'
+
+      const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?q=${long}&units=metric&appid=96167910369f1228a67e094ac8e334b6`;
+
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const iconImg = data.weather[0].icon;
+          const temperature = data.main.temp;
+          const summary = data.weather[0].description;
+          console.log(data);
+          tempDescription.textContent = summary.charAt(0).toUpperCase() + summary.slice(1);
+          tempDegree.textContent = temperature;
+          locationTimeZone.textContent = data.name;
+          icon.src = `http://openweathermap.org/img/wn/${iconImg}@2x.png`
+        })
+    });
+  }
+});
