296a2fd0f09fcf455c47c93b9282f85b13def9ba
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-11-06 23:22:34 -0500

    Add webpack server Fix linters

--- src/index.js
+++ src/index.js
@@ -1,34 +1,43 @@
 import './style.scss';
 
 window.addEventListener('load', () => {
-  let long;
-  let tempDescription = document.querySelector('.temperature-description');
-  let tempDegree = document.querySelector('.temperature-degree');
-  let locationTimeZone = document.querySelector('.location-timezone');
-  let icon = document.getElementById('icon');
+  const button = document.getElementById('submit');
+  const tempDescription = document.querySelector('.temperature-description');
+  const tempDegree = document.querySelector('.temperature-degree');
+  const locationTimeZone = document.querySelector('.location-timezone');
+  const icon = document.getElementById('icon');
+  const container = document.querySelector('.weather-cont');
+  container.style.display = 'none';
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      long = 'medellin';
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
 
-      const proxy = 'http://cors-anywhere.herokuapp.com/'
+    const city = document.getElementById('city').value;
 
-      const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?q=${long}&units=metric&appid=96167910369f1228a67e094ac8e334b6`;
+    const proxy = 'http://cors-anywhere.herokuapp.com/';
 
-      fetch(api)
-        .then(response => {
-          return response.json();
-        })
-        .then(data => {
-          const iconImg = data.weather[0].icon;
-          const temperature = data.main.temp;
-          const summary = data.weather[0].description;
-          console.log(data);
-          tempDescription.textContent = summary.charAt(0).toUpperCase() + summary.slice(1);
-          tempDegree.textContent = temperature;
-          locationTimeZone.textContent = data.name;
-          icon.src = `http://openweathermap.org/img/wn/${iconImg}@2x.png`
-        })
-    });
-  }
+    const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=96167910369f1228a67e094ac8e334b6`;
+
+    fetch(api)
+      .then(response => response.json())
+      .then(data => {
+        const container = document.querySelector('.weather-cont');
+        container.style.display = 'block';
+        const iconImg = data.weather[0].icon;
+        const temperature = data.main.temp;
+        const summary = data.weather[0].description;
+        tempDescription.textContent = summary.charAt(0).toUpperCase() + summary.slice(1);
+        tempDegree.textContent = temperature;
+        locationTimeZone.textContent = data.name;
+        icon.src = `http://openweathermap.org/img/wn/${iconImg}@2x.png`;
+      })
+      .catch(data => {
+        console.log(data);
+        const container = document.querySelector('.weather-cont');
+        const errorDiv = document.createElement('h1');
+        errorDiv.textContent = 'Please enter a valid city';
+
+        container.appendChild(errorDiv);
+      })
+  });
 });
